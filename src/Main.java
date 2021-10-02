46f7c68072a750af53bb4e65fbe7ce41e0f2f586
evgeniaalejnikova <evgeniaalejnikova@MacBook-Air-Evgenia.local>
2021-10-02 10:41:02 +0300

    All files added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int accountBalance = 2_000_000_000;
+        int transferAmount = 500_000_000;
+        int totalSum = accountBalance + transferAmount;
+        System.out.println(totalSum);
+    }
+}
